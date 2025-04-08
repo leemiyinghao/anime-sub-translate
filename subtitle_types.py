39c20744b50c01946e58cb8d8f31e1e70a71171b
catLee <leemiyinghao@gmx.com>
2025-04-09 06:13:49 +0800

    feat: Add subtitle type hints

---
+++ subtitle_types.py
@@ -0,0 +1,21 @@
+# type hint for subtitle format
+from typing import Optional
+from typing_extensions import TypedDict
+
+
+class SubtitleDialogue(TypedDict):
+    id: int
+    content: str
+
+
+class RichSubtitleDialogue(SubtitleDialogue):
+    id: int
+    content: str
+    actor: Optional[str]
+    style: Optional[str]
+
+
+class PreTranslatedContext(TypedDict):
+    original: str
+    translated: str
+    description: str
